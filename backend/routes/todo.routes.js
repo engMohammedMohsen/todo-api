9182fe9161c8cc16c654db3e534137530edba882
Mohammed Mohsen <mohamedmohsan142@gmail.com>
2025-01-18 19:35:49 +0200

    rename validator

--- backend/routes/todo.routes.js
+++ backend/routes/todo.routes.js
@@ -5,10 +5,20 @@ const {
   editTodo,
   deleteTodo,
 } = require("../controllers/todo.controller");
+
+const validationExecution = require("../middlewares/validationExecution");
+const { todoValidator } = require("../utils/validator/addTodoValidator");
 const router = express.Router();
 
-router.route("/").get(getTodo).post(addTodo);
+router
+  .route("/")
+  .get(getTodo)
+  .post(todoValidator(), validationExecution, addTodo);
 
-router.route("/:todoId").get(getTodo).patch(editTodo).delete(deleteTodo);
+router
+  .route("/:todoId")
+  .get(getTodo)
+  .patch(todoValidator(), validationExecution, editTodo)
+  .delete(deleteTodo);
 
 module.exports = router;
